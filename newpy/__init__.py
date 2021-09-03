c5ecfba514f64d9c9611f8702f20186b4a45108d
janithPet <jpetangoda@gmail.com>
2021-09-03 08:37:55 +0100

    whoops

--- newpy/__init__.py
+++ newpy/__init__.py
@@ -5,7 +5,5 @@ import pathlib
 
 from newpy.loggers import ColoredFormatter
 
-print(str(pathlib.Path(__file__).parent.resolve()))
-print(os.listdir(str(pathlib.Path(__file__).parent.resolve())))
 logging.config.fileConfig(str(pathlib.Path(__file__).parent.resolve()) + "/logging_config.ini")
 logger = logging.getLogger()
