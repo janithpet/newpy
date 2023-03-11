99e422786f2f710e0ea8deab90c14353c74ceb7c
janithPet <jpetangoda@gmail.com>
2023-03-11 03:01:10 +0000

    pyenv now works, and better logging of subprocess errors

--- newpy/newpy.py
+++ newpy/newpy.py
@@ -98,6 +98,21 @@ retrieve_author = partial(retrieve, arg="author", tmp=True)
 retrieve_manager = partial(retrieve, arg="manager", tmp=False)
 retrieve_license = partial(retrieve, arg="license", tmp=True)
 
+def handle_subprocess_result(result: subprocess.CompletedProcess, process_description: str, log_if_successful: bool = False) -> bool:
+	if result is None:
+		return
+
+	if result.stderr != "":
+		if result.returncode != 0:
+			logger.error(f"Could not {process_description}")
+			logger.error(f"{result.stderr.strip()}")
+		else:
+			logger.warning(f"Could not {process_description}")
+			logger.warning(f"{result.stderr.strip()}")
+	if result.stdout != "" and log_if_successful:
+		if result.returncode == 0:
+			logger.info(result.stdout.strip())
+
 
 def update_template(d: Dict, project_name, author, project_path):
     for key in d.keys():
@@ -174,47 +189,58 @@ def main():
         assert os.getcwd() == project_path
 
         if arguments.venv:
+            result = None
+            _venv_name = current_dir_name + "." + (arguments.venv_name if arguments.venv_name else project_name)
+
             logger.info(f"Installing virtual environment")
             if manager == "pipenv":
-                subprocess.run(["pipenv install"], shell=True)
+                result = subprocess.run(["pipenv install"], shell=True, capture_output=True, text=True)
             elif manager == "pip":
-                _venv_name = arguments.venv_name if arguments.venv_name else ".venv"
-                subprocess.run([f"virtualenv {_venv_name}"], shell=True)
+                # _venv_name = arguments.venv_name if arguments.venv_name else ".venv"
+                result = subprocess.run([f"virtualenv {_venv_name}"], shell=True, capture_output=True, text=True)
             elif manager == "pyenv":
-                _venv_name = current_dir_name + "." + (arguments.venv_name if arguments.venv_name else project_name)
-                print(_venv_name)
-                subprocess.run([f"pyenv virtualenv {_venv_name};echo {_venv_name} > .python-version"], shell=True)
+                # _venv_name = current_dir_name + "." + (arguments.venv_name if arguments.venv_name else project_name)
+                result = subprocess.run([f"pyenv virtualenv {_venv_name};echo {_venv_name} > .python-version"], shell=True, capture_output=True, text=True)
+
+            handle_subprocess_result(result, "create virtual environment")
 
 
         if arguments.install:
+            result = None
             logger.info(f"Installing current project using {manager}")
             if arguments.venv:
                 if manager == "pipenv":
-                    subprocess.run(["bash", "-c", "source $(pipenv --venv)/bin/activate && pipenv install -e . && exit"])
+                    result = subprocess.run(["bash", "-c", "source $(pipenv --venv)/bin/activate && pipenv install -e . && exit"], capture_output=True, text=True)
                 elif manager == "pip":
                     _venv_name = arguments.venv_name if arguments.venv_name else ".venv"
-                    subprocess.run(["bash", "-c", f"source {_venv_name}/bin/activate && pip install -e . && exit"])
+                    result = subprocess.run(["bash", "-c", f"source {_venv_name}/bin/activate && pip install -e . && exit"], capture_output=True, text=True)
                 elif manager == "pyenv":
-                    print(f"Cannot install project with pyenv. Please run\n\tcd {project_name}; pip install -e .")
-            else:
-                subprocess.run([f"{manager} install -e ."], shell=True)
+                    result = subprocess.run(["pip install -e ."], shell=True, capture_output=True, text=True)
+            # else:
+            #     subprocess.run([f"{manager} install -e ."], shell=True, capture_output=True, text=True)
+            handle_subprocess_result(result, "install project", False)
 
         if arguments.git:
+            result = None
             logger.info("Initialising git")
-            subprocess.run(["git init"], shell=True)
-            subprocess.run(["git add ."], shell=True)
-            subprocess.run(["git commit -am 'first commit'"], shell=True)
+            subprocess.run(["git init"], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(["git add ."], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(["git commit -aqm 'first commit'"], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
             if arguments.precommit:
                 logger.info("Installing pre-commit hooks")
                 if manager == "pipenv":
-                    subprocess.run(["bash", "-c", "source $(pipenv --venv)/bin/activate && pre-commit install && exit"])
+                    result = subprocess.run(["bash", "-c", "source $(pipenv --venv)/bin/activate && pre-commit install && exit"], capture_output=True, text=True)
                 elif manager == "pip":
                     _venv_name = arguments.venv_name if arguments.venv_name else ".venv"
-                    subprocess.run(["bash", "-c", f"source {_venv_name}/bin/activate && pre-commit install && exit"])
+                    result = subprocess.run(["bash", "-c", f"source {_venv_name}/bin/activate && pre-commit install && exit"], capture_output=True, text=True)
                 elif manager == "pyenv":
-                    _venv_name = arguments.venv_name if arguments.venv_name else project_name
-                    subprocess.run(["bash", "-c", f"pyenv activate {_venv_name}  && pre-commit install && exit"])
+                    _venv_name = current_dir_name + "." + (arguments.venv_name if arguments.venv_name else project_name)
+                    result = subprocess.run([f"pre-commit install"], shell=True, capture_output=True, text=True)
+            
+                handle_subprocess_result(result, "setup pre-commit")
+        
+                    
 
     else:
         logger.error("Please enter a project_name")
